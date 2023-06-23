3ce639bd1c94ba60399b58001ea95e7f7a1c7ceb
na0nie <naonchun@gmail.com>
2023-06-23 11:13:06 -0700

    updated meta tag image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1031,20 +1031,20 @@ const Home: NextPage = () => {
           <meta
             name="twitter:image"
             key="twitterimg"
-            content="https://lapd-arrests-2019.lacontroller.io/building-map.png"
+            content="https://2019arrests.lacontroller.io/lapd-arrests-2019.png"
           ></meta>
           <meta name="description" content="LAPD Arrests 2019." />
 
           <meta
             property="og:url"
-            content="https://lapd-arrests-2019.lacontroller.io"
+            content="https://2019arrests.lacontroller.io"
           />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="LAPD Arrests 2019 | Map" />
           <meta property="og:description" content="LAPD Arrests 2019." />
           <meta
             property="og:image"
-            content="https://lapd-arrests-2019.lacontroller.io/building-map.png"
+            content="https://2019arrests.lacontroller.io/lapd-arrests-2019.png"
           />
         </Head>
 
